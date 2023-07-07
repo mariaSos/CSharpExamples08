cf2029f92bae54a426723ccdc820d85eb2a0beb7
maria_s_pr@mail.ru <maria_s_pr@mail.ru>
2023-07-07 20:53:51 +0500

    Задача 58

--- Ex08_3/Program.cs
+++ Ex08_3/Program.cs
@@ -1,6 +1,13 @@
 ﻿// Задача 58: Задайте две матрицы. Напишите программу, 
 // которая будет находить произведение двух матриц.
 
+//Ввод данных
+int[] ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse); ;
+}
+
 //Печать массива
 void PrintMatrix(int[,] matrix)
 {
@@ -14,11 +21,30 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+//Генерация массива
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    var rand = new Random();
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return tempMatrix;
+}
+
 //Умножение матриц
 int[,] MultiMatrix(int[,] matrix1, int[,] matrix2)
 {
-    int size = matrix1.GetLength(0);
-    var resultMatrix = new int[size, size];
+    // int size = matrix1.GetLength(0);
+    int row = matrix1.GetLength(0);
+    int col = matrix2.GetLength(1);
+
+    var resultMatrix = new int[row, col];
     for (int i = 0; i < matrix1.GetLength(0); i++)
     {
         for (int j = 0; j < matrix2.GetLength(1); j++)
@@ -31,12 +57,37 @@ int[,] MultiMatrix(int[,] matrix1, int[,] matrix2)
     }
     return resultMatrix;
 }
+bool ValidMulti(int col1, int row2)
+{
+    if (col1 != row2)
+    {
+        return false;
+    }
+    else
+        return true;
+}
 
 ///..............................................
-int[,] matrix1 = { { 2,4 }, { 3, 2} };
-int[,] matrix2 = { {3,4},{3,3}};
+//int[,] matrix1 = { { 2,4 }, { 3, 2} };
+//int[,] matrix2 = { {3,4},{3,3}};
 
-int[,] matrix = MultiMatrix(matrix1, matrix2);
+int[] size1 = ReadInt("Введите размер первой матрицы через запятую: ");
+int[] size2 = ReadInt("Введите размер второй матрицы через запятую: ");
 
-PrintMatrix(matrix);
+if (ValidMulti(size1[1], size2[0]))
+{
+    int[] rang1 = ReadInt("Введите левую и правую границы первой матрицы через запятую: ");
+    int[] rang2 = ReadInt("Введите левую и правую границы второй матрицы через запятую: ");
+    //Генерация массива
 
+    int[,] matrix1 = FillMatrix(size1[0], size1[1], rang1[0], rang1[1]);
+    int[,] matrix2 = FillMatrix(size2[0], size2[1], rang2[0], rang2[1]);
+
+    int[,] multiMatrix = MultiMatrix(matrix1, matrix2);
+    
+    PrintMatrix(multiMatrix);
+}
+else
+{
+    System.Console.WriteLine("Умножение заданных матриц не возможно!");
+}
